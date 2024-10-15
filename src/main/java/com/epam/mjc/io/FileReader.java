3ed788c8a62cbd559fc7e632d1a48210d091692d
1 <test@tut.by>
2024-10-15 14:52:43 +0300

    commit changes

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) {
+
+        Map<String, String> dataMap = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] keyValue = line.split(": ");
+                dataMap.put(keyValue[0].trim(), keyValue[1].trim());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Profile(dataMap.get("Name"),Integer.parseInt(dataMap.get("Age")),dataMap.get("Email"),Long.parseLong(dataMap.get("Phone")));
     }
+
+    /*public static void main(String[] args) {
+        String directory ="resources";
+        String fileName = "Profile.txt";
+        String absolutePath = directory + File.separator + fileName;
+        File file = new File(absolutePath);
+        Profile profile = getDataFromFile(file);
+        System.out.println(profile);
+    }*/
 }
